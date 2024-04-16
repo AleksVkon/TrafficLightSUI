9a916d11ed2077a7c718b44d4d89c5275647a883
Aleksey Konchyts <konchits.aleksey@gmail.com>
2024-04-16 11:38:26 +0300

    Initial Commit

---
+++ TrafficLightSUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrafficLightSUI
+//
+//  Created by Aleksey Konchyts on 16.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrafficLightSUI/TrafficLightSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSUIApp.swift
+//  TrafficLightSUI
+//
+//  Created by Aleksey Konchyts on 16.04.24.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
